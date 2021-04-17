47b6c1b181b7368b7fb7d64fa2894133bdc0ac54
Jan F. Bausch <mail@jfbausch.de>
2021-04-17 14:04:25 +0200

    Add scraper

--- scraper.py
+++ scraper.py
@@ -1,54 +1,65 @@
-from typing import List
-from lxml import html, etree
+from datetime import date, timedelta
+import csv
 import requests
+import wikitextparser
 
-class ScrapedIncident():
-    def __init__(self, year: int, month: str, date: int, description: str) -> None:
-        self.year = year
-        self.month = month
-        self.date = date
-        self.description = description
+def scrape_all():
+    start = date(2010, 1, 1)
+    end = date(2020, 1, 1)
+    dates = [start + timedelta(days=x) for x in range((end - start).days + 1)]
+    return [scrape_date(date) for date in dates]
 
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
-
-months = ["December"]
-
-def scrape_all() -> List[ScrapedIncident]:
-    years = list(range(2020, 2020 + 1))
-    return [scrape_month(y, m) for y in years for m in months]
-
-
-def print_nodes(nodes):
-    print([etree.tostring(x, pretty_print=True) for x in nodes])
-
-def scrape_month(year: int, month: str) -> List[ScrapedIncident]:
-    print('Scraping', year, month)
-    url = f'https://en.m.wikipedia.org/wiki/Portal:Current_events/{month}_{year}'
+def scrape_date(date):
+    timestamp = date.strftime('%Y_%B_%-d')
+    print(timestamp)
+    url = f'https://en.wikipedia.org/wiki/Portal%3aCurrent_events/{timestamp}?action=raw'
     page = requests.get(url)
-    tree = html.fromstring(page.content)
-    dayDivs = tree.xpath('//div[@class="vevent"]//div[@class="description"]')
-    return [scrape_day(dayDiv) for dayDiv in dayDivs]
+    source = page.content.decode('utf-8')
+    try:
+        news_items = parse_date(source)
+        news_items_with_date = [[date, item] for item in news_items]
+        return news_items_with_date
+    except Exception as e:
+        print('Error', e)
+        return None
 
-def scrape_day(node) -> List[ScrapedIncident]:
-    dailyDivs = node.xpath('.//b[text()="Armed conflicts and attacks"]')
-    print(len(dailyDivs))
-    return ScrapedIncident(2929, 'January', 1, 'df')
+def parse_date(source):
+    left = '<!-- All news items below this line -->'
+    right = '<!-- All news items above this line -->'
+    extract = source[source.index(left) + len(left) : source.index(right)]
+    parsed = wikitextparser.parse(extract)
+    parsed_lists = parsed.get_lists()
+    try:
+        category = 'Armed conflicts and attacks'
+        index = [x.items[0] for x in parsed_lists].index(category)
+        news_items = parsed_lists[index + 1]
+        return parse_news_items(news_items)
+    except ValueError:
+        return []
 
-def export_to_file(incidents: List[ScrapedIncident]) -> None:
-    return None
+def parse_news_items(news_items):
+    result = []
+    for index, item in enumerate(news_items.items):
+        try:
+            subitems = news_items.sublists(index)[0].items
+            subitems_with_prefix = [f'{item} {subitem}' for subitem in subitems]
+            result.extend(subitems_with_prefix)
+        except IndexError:
+            result.append(item)
+
+    result = [wikitextparser.parse(item).plain_text() for item in result]
+
+    print(len(result), 'items found')
+    return result
+
+def export_to_csv(results):
+    with open('export.csv', mode='w') as csv_file:
+        writer = csv.writer(csv_file)
+        for item in results:
+            if item != None:
+                writer.writerows(item)
+    
 
 if __name__ == "__main__":
-    scrape_all()
+    results = scrape_all()
+    export_to_csv(results)
