92847a31fc56b23abe4f1b1d0939af3ef3876b28
Jan F. Bausch <mail@jfbausch.de>
2021-08-01 13:01:58 +0200

    Add jsonifier.py

---
+++ tools/jsonifier.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import json
+import argparse
+from hashlib import sha256
+from pathlib import Path
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)
+    args = parser.parse_args()
+    path = args.path
+
+    with open(path, "w") as text_file:
+        text_file.write('{"incidents":[')
+        for line in sys.stdin:
+            text_file.write(line + ',')
+        text_file.write(']}')
+
+if __name__ == "__main__":
+    main()
