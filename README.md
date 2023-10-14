8f325fabd4552a834693aaa1020c28c96e7f77b5
Jan F. Bausch <mail@jfbausch.de>
2023-10-14 16:44:22 +0200

    Add link to thesis

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Visualization of armed conflicts from Wikipedia
 
-> A web application for the bachelor's thesis on »Geospatial visualization of Wikipedia news reports about armed conflicts«
-
+> A web application for the bachelor's thesis on »[Geospatial visualization of Wikipedia news reports about armed conflicts](https://raw.githubusercontent.com/Global-Conflicts/thesis/master/Thesis.pdf)«
 
 <p align="center">
    <img src="screenrecording.gif" alt="Screenshot" width="70%">
--- frontend/src/About.js
+++ frontend/src/About.js
@@ -8,7 +8,7 @@ const About = ({ close }) => (
     <p>The Current Events Portal summarizes current news incidents on a daily basis, together with links to sources and related Wikipedia articles.</p>
     <p>The Portal's <a href="https://en.wikipedia.org/wiki/Wikipedia:How_the_Current_events_page_works">guidlines</a> strive for <a href="https://en.wikipedia.org/wiki/Wikipedia:Neutral_point_of_view">neutral reporting</a> and citing <a href="https://en.wikipedia.org/wiki/Wikipedia:Reliable_sources">reliable sources</a>. </p>
     <p>Copyright by Wikipedia and Wikidata editors and contributors under the under the terms of the <a href="https://en.wikipedia.org/wiki/Wikipedia:Text_of_the_Creative_Commons_Attribution-ShareAlike_4.0_International_License">Creative Commons 4.0 License</a> and, unless otherwise noted, the <a href="https://en.wikipedia.org/wiki/Wikipedia:Text_of_the_GNU_Free_Documentation_License">GNU Free Documentation License.</a></p>
-    <p>This project is developed by <a href="https://jan-bausch.com/">Jan Bausch</a>, originally as part of a University thesis called &#187;Geospatial visualization of Wikipedia news reports about armed conflicts&#171;.</p>
+    <p>This project is developed by <a href="https://jan-bausch.com/">Jan Bausch</a>, originally as part of a University thesis called &#187;<a href="https://raw.githubusercontent.com/Global-Conflicts/thesis/master/Thesis.pdf">Geospatial visualization of Wikipedia news reports about armed conflicts</a>&#171;.</p>
     <p>The source code can be found on <a href="https://github.com/Global-Conflicts/global-conflicts">Github</a>.</p>
   </>
 );
